0944065793ae59ff2ec095f9dff50298fbf40ae3
ciaraosull <ciaraosull@gmail.com>
2022-05-06 10:34:38 +0000

    add draft function view to test url for home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# import any models from classes created
 
-# Create your views here.
+
+def home(request):
+    """ draft function view to test linking of url """
+    return HttpResponse('<h1>Home Page</h1>')
