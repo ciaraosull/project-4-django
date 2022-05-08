254fc53a299864135d4bed9d97823f01ce1d58d5
ciaraosull <ciaraosull@gmail.com>
2022-05-08 21:31:29 +0000

    Install summernote and add to description field for admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 """ Imports for blog admin"""
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
-admin.site.register(Post)
 
-
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """ Class to place what field summernote to be displayed in """
+    summernote_fields = ('project_description')
