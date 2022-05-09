d4231cf1c95dc9df3017f108b13160bc88a62220
ciaraosull <ciaraosull@gmail.com>
2022-05-09 21:29:33 +0000

    Add blog views for posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-# from django.views import generic
-# import any models from classes created
+from django.views import generic
+from .models import Post
 
 
-def home(request):
-    """ draft function view to test linking of url """
-    return render(request, 'index.html')
+# def home(request):
+#     """ draft function view to test linking of url """
+#     return render(request, 'index.html')
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-date_posted')
+    template_name = 'index.html'
+    paginate_by = 6
