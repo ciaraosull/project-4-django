6b90c6e3ad2d492e964a956399a8577b0cac9f05
ciaraosull <ciaraosull@gmail.com>
2022-05-09 10:19:01 +0000

    Add appcomments class to admin and aove comcomment method

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 """ Imports for blog admin"""
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from .models import Post, Comments
 
 
 @admin.register(Post)
@@ -12,3 +12,14 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('project_title', )}
     list_filter = ('date_posted', 'updated_on')
     summernote_fields = ('project_description')
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'date_posted', 'approved')
+    list_filter = ('approved', 'date_posted')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
