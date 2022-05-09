e5d3a8ee686c31f8b6f5a9b65f4f1ba7ef4935da
ciaraosull <ciaraosull@gmail.com>
2022-05-09 09:17:01 +0000

    Rename title in model to clarify for user project title

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     """ Model for Posts """
-    title = models.CharField(max_length=200, unique=True)
+    project_title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_post')
