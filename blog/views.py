2f0685ba178f4e3bd77afad76ec19ca1bbd62c66
ciaraosull <ciaraosull@gmail.com>
2022-05-09 22:42:29 +0000

    Add postlistview and insert some model fields into index.html to test

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView
 from .models import Post
 
 
-# def home(request):
-#     """ draft function view to test linking of url """
-#     return render(request, 'index.html')
+def home(request):
+    """ draft function view to test linking of url """
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'index.html', context)
 
-class PostList(generic.ListView):
+class PostListView(ListView):
     model = Post
-    queryset = Post.objects.order_by('-date_posted')
     template_name = 'index.html'
-    paginate_by = 6
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
