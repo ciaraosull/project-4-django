77754a7834c29cf2613767948daff550fce5b12f
ciaraosull <ciaraosull@gmail.com>
2022-05-09 09:47:18 +0000

    Add display and search to admin post

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,8 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """ Class admin site post """
+    list_display = ('project_title', 'slug', 'author', 'date_posted')
+    search_fields = ['project-title', 'project_description']
     prepopulated_fields = {'slug': ('project_title', )}
-    list_filter = ('date_posted', 'likes')
+    list_filter = ('date_posted', 'updated_on')
     summernote_fields = ('project_description')
