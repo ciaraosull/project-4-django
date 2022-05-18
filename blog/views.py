058f53f11412bb1bb03457a219cdaa1c72538427
ciaraosull <ciaraosull@gmail.com>
2022-05-18 20:37:30 +0000

    Set author to logged in author before adding post

--- blog/views.py
+++ blog/views.py
@@ -25,9 +25,15 @@ class PostCreateView(CreateView):
     """ Class to allow users to create posts """
     model = Post
     fields = [
-        'project_title', 
-        'project_description', 
-        'deployed_link', 
-        'code_repository', 
+        'project_title',
+        'slug',
+        'project_description',
+        'deployed_link',
+        'code_repository',
         'other_relevant_information'
         ]
+
+    def form_valid(self, form):
+        """Set the author to the logged in user"""
+        form.instance.author = self.request.user  # set author to loggedin user
+        return super().form_valid(form)  # then submit form
