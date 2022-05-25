83a7450912bd56999ecedbb3b833c5e14c4b748e
ciaraosull <ciaraosull@gmail.com>
2022-05-25 19:38:51 +0000

    Install allauth & add to settings & url

--- codesea/settings.py
+++ codesea/settings.py
@@ -43,14 +43,23 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'crispy_forms',
     'blog',
     'profiles',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
