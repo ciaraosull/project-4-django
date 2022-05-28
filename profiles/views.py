b292342112bb5a06f4a8308b9586d008b675c544
ciaraosull <ciaraosull@gmail.com>
2022-05-28 22:04:46 +0000

    Add login required decorator to ensure redirect to login for profile url

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required  # decorator to ensure user is logged in to view this page
 def profile(request):
     """Function to return the users profile template"""
     return render(request, 'profiles/profile-page.html')
