f97a2983cd0ffa9e980a1819330311546f5ef309
ciaraosull <ciaraosull@gmail.com>
2022-06-21 22:23:02 +0000

    Change model default image to cloudinary and insert profile image url in template for profile

--- profiles/models.py
+++ profiles/models.py
@@ -3,11 +3,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+DEFAULT_IMG = "https://res.cloudinary.com/ciara0s1980/image/upload/v1655849315/default__image.webp"
+
 
 class Profile(models.Model):
     """ Model for Profile """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = CloudinaryField('image', blank='True')
+    profile_image = CloudinaryField('image', default=DEFAULT_IMG)
 
     def __str__(self):
         """ To return the user's username object as a string """
